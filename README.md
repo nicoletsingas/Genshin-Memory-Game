dcf93c196ab8599ea391735c5b5dc7b774f9520b
Nicole Tsingas <nicolecass27@gmail.com>
2023-11-24 12:40:31 -0300

    Criado projeto

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Genshin-Memory-Game2
+# Genshin-Memory-Game
+>Desenvolvimento de Jogos - DIO
+
+# Índice
+
+* [1. Resumo do projeto](#1-resumo-do-projeto)
+* [2. Deploy](#2-deploy)
+* [3. Tecnologias Utilizadas](#3-tecnologias-utilizadas)
+
+
+## 1. Resumo do projeto📝
+Projeto desenvolvido para o curso de Desenvolvimento de Jogos com JavaScript da DIO em parceria com Ifood e Potencia Tech. Foi desenvolvido um jogo de memória com os personagens de Genshin Impact. O usuario deve acertar todos os pares para finalizar o jogo. É possivel resetar o game a qualquer momento.
+
+## 2. Deploy📝
+### 📎[Acesse aqui](https://nicoletsingas.github.io/Genshin-Memory-Game/)
+![preview](src/assets/preview1.png)
+
+## 3. Tecnologias Utilizadas✅ 
+<img alt="HTML" height="50" src="https://cdn2.iconfinder.com/data/icons/designer-skills/128/code-programming-html-markup-develop-layout-language-512.png"> <img alt="CSS" height="50" src="https://cdn2.iconfinder.com/data/icons/designer-skills/128/code-programming-css-style-develop-layout-language-512.png"> <img alt="JS" height="50" src="https://cdn2.iconfinder.com/data/icons/designer-skills/128/code-programming-javascript-software-develop-command-language-256.png"> <img alt="git" height="40" src="https://cdn3.iconfinder.com/data/icons/social-media-2169/24/social_media_social_media_logo_git-256.png"/> <img alt="github" height="45" src="https://cdn1.iconfinder.com/data/icons/unicons-line-vol-3/24/github-256.png"/>
+
+
---
+++ src/engine.js
@@ -0,0 +1,140 @@
+const characters = [
+  {
+    imgChar: '../src/assets/ayato.png',
+  },
+  {
+    imgChar: '../src/assets/ayato.png',
+  },
+  {
+    imgChar: '../src/assets/cyno.png',
+  },
+  {
+    imgChar: '../src/assets/cyno.png',
+  },
+  {
+    imgChar: '../src/assets/kaeya.png',
+  },
+  {
+    imgChar: '../src/assets/kaeya.png',
+  },
+  {
+    imgChar: '../src/assets/kazuha.png',
+  },
+  {
+    imgChar: '../src/assets/kazuha.png',
+  },
+  {
+    imgChar: '../src/assets/kokomi.png',
+  },
+  {
+    imgChar: '../src/assets/kokomi.png',
+  },
+  {
+    imgChar: '../src/assets/lyney.png',
+  },
+  {
+    imgChar: '../src/assets/lyney.png',
+  },
+  {
+    imgChar: '../src/assets/nahida.png',
+  },
+  {
+    imgChar: '../src/assets/nahida.png',
+  },
+  {
+    imgChar: '../src/assets/noelle.png',
+  },
+  {
+    imgChar: '../src/assets/noelle.png',
+  },
+  {
+    imgChar: '../src/assets/raiden.png',
+  },
+  {
+    imgChar: '../src/assets/raiden.png',
+  },
+  {
+    imgChar: '../src/assets/shenhe.png',
+  },
+  {
+    imgChar: '../src/assets/shenhe.png',
+  },
+  {
+    imgChar: '../src/assets/tighnari.png',
+  },
+  {
+    imgChar: '../src/assets/tighnari.png',
+  },
+  {
+    imgChar: '../src/assets/traveler.png',
+  },
+  {
+    imgChar: '../src/assets/traveler.png',
+  },
+  {
+    imgChar: '../src/assets/xiangling.png',
+  },
+  {
+    imgChar: '../src/assets/xiangling.png',
+  },
+  {
+    imgChar: '../src/assets/zhongli.png',
+  },
+  {
+    imgChar: '../src/assets/zhongli.png',
+  },
+  {
+    imgChar: '../src/assets/qiqi.png',
+  },
+  {
+    imgChar: '../src/assets/qiqi.png',
+  },
+  {
+    imgChar: '../src/assets/traveler2.png',
+  },
+  {
+    imgChar: '../src/assets/traveler2.png',
+  },
+];
+let openCards = [];
+
+const randomChar = characters.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i = 0; i < characters.length; i++){
+  const cardContainer = document.createElement('div');
+  cardContainer.classList.add('item');
+
+  const img = document.createElement('img');
+  img.src = randomChar[i].imgChar;
+  cardContainer.appendChild(img);
+  cardContainer.onclick = randomClick;
+
+  document.querySelector('.game').appendChild(cardContainer);
+}
+
+function randomClick() {
+  if(openCards.length < 2){
+    this.classList.add('openCard');
+    openCards.push(this);
+  }
+
+  if(openCards.length === 2){
+    setTimeout(checkMatchCards, 500);
+  }
+}
+
+function checkMatchCards() {
+  if(openCards[0].innerHTML === openCards[1].innerHTML){
+    openCards[0].classList.add('matchCards');
+    openCards[1].classList.add('matchCards');
+  } else {
+    openCards[0].classList.remove('openCard');
+    openCards[1].classList.remove('openCard');
+  }
+
+  openCards = [];
+
+  if(document.querySelectorAll('.openCard').length === characters.length){
+    alert('Congratulations!! You found all the pairs, Paimon is proud of you!');
+  }
+}
